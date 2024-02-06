c032d9b93438bd2fc614f677fde214df775e11da
Jetom88 <shinhro3o@gmail.com>
2024-02-06 22:14:29 +0900

    feat: metadata 다루는 방법

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,3 +1,10 @@
+// page의 head 부분에 표시되는 내용
+// 메타데이터는 중첩이 되는것이 아니라 합쳐짐
+// 클라이언트 컴포넌트에서는 메타데이터를 내보낼 수 없고, 서버 컴포넌트에서만 있을 수 있음
+export const metadata = {
+  title: "Home",
+};
+
 export default function Page() {
   return (
     <div>
