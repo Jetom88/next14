4ebe3f03497a1f7a1dabd575a436532fcbf6a3a8
Jetom88 <shinhro3o@gmail.com>
2024-02-06 21:40:52 +0900

    feat: layout 기능 실습

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,6 @@
-import Navigation from "../components/navigation";
-
-// root page
 export default function Page() {
   return (
     <div>
-      <Navigation />
       <h1>Hello!</h1>
     </div>
   );
---
+++ app/about-us/company/jobs/sales/page.tsx
@@ -0,0 +1,4 @@
+// layout은 중첩 될 뿐 상쇄 되지않음
+export default function Sales() {
+  return <h1>Sales Jobs</h1>;
+}
