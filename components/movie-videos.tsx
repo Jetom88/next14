e6a765799fd6ddd571d573c2d5624167955783ca
Jetom88 <shinhro3o@gmail.com>
2024-03-05 16:17:00 +0900

    feat: error 처리 방법

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,8 +1,11 @@
 import { API_URL } from "../app/(home)/page";
 
 async function getVideos(id: string) {
-  const res = await fetch(`${API_URL}/${id}/videos`);
-  return res.json();
+  // const res = await fetch(`${API_URL}/${id}/videos`);
+  // return res.json();
+
+  await new Promise((res) => setTimeout(res, 3000));
+  throw new Error("error");
 }
 
 //video만 render하는 component
