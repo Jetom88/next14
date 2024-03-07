ab699f47b090e6fb766fdca9a1d1ba0cd1aab9bb
Jetom88 <shinhro3o@gmail.com>
2024-03-07 16:30:10 +0900

    feat: 동적으로 metadata head 만드는 방법

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,14 +1,21 @@
 import { Suspense } from "react";
-import MovieInfo from "../../../../components/movie-info";
+import MovieInfo, { getMovie } from "../../../../components/movie-info";
 import MovieVideos from "../../../../components/movie-videos";
+
+interface IParams {
+  params: { id: string };
+}
+
+export async function generateMetadata({ params: { id } }: IParams) {
+  const movie = await getMovie(id);
+  return {
+    title: movie.title,
+  };
+}
 
 // Suspense는 component가 await되는 동안 표시할 메세지를 render할 수 있게 해줌
 
-export default async function MovieDetail({
-  params: { id },
-}: {
-  params: { id: string };
-}) {
+export default async function MovieDetailPage({ params: { id } }: IParams) {
   return (
     <div>
       {/* suspense를 사용함에 따라 어느 부분이 로딩 상태여야 하는지 명시해줄 수 있게 됨*/}
